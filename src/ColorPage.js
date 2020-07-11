d57e9ff0f1982dc1754ca0f05de8e20894a5dac6
marco mariscal <marco.a.mariscal@gmail.com>
2020-07-11 17:17:41 -0400

    adding render method to colorPage route

--- src/ColorPage.js
+++ src/ColorPage.js
@@ -4,12 +4,11 @@ import "./ColorPage.css";
 
 const ColorPage = ({ color, history }) => {
   if (color === undefined) history.push("/colors");
-  console.log(color);
 
   return (
     <div className="ColorPage" style={{ backgroundColor: color.value }}>
       <h1>{color.name}!</h1>
-      <Link exact path to="/colors">
+      <Link to="/colors">
         <h1>Go Back</h1>
       </Link>
     </div>
--- src/Routes.js
+++ src/Routes.js
@@ -1,32 +1,22 @@
 import React, { useState } from "react";
-import {
-  Route,
-  Switch,
-  Redirect,
-  useParams,
-  useHistory,
-} from "react-router-dom";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 import ColorPickerForm from "./ColorPickerForm";
 import ColorList from "./ColorList";
 import ColorPage from "./ColorPage";
 
 const Routes = () => {
   const [colors, setColors] = useState([]);
-  const [currColor, setCurrColor] = useState({});
 
-  const { name } = useParams();
   const history = useHistory();
 
   const addColor = (color) => {
     setColors((colors) => [...colors, color]);
   };
 
-  const findColor = () => {
-    for (const color of colors) {
-      if (color.name === name) {
-        setCurrColor(color);
-      }
-    }
+  const renderColor = (props) => {
+    const { name } = props.match.params;
+    const color = colors.find((color) => color.name === name);
+    return <ColorPage {...props} color={color} history={history} />;
   };
 
   return (
@@ -38,9 +28,7 @@ const Routes = () => {
         <Route exact path="/colors/new">
           <ColorPickerForm addColor={addColor} />
         </Route>
-        <Route path="/colors/:name" render={findColor}>
-          <ColorPage color={currColor} history={history} />;
-        </Route>
+        <Route path="/colors/:name" render={renderColor}></Route>
         <Redirect to="/colors" />
       </Switch>
     </div>
